79d1e8494638fe4f4ee3055d3b30c356f62d1a92
kop-san <anouson11103@gmail.com>
2024-10-27 00:11:26 +0700

    todo list on flutter app v0.1

---
+++ lib/constrants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdBlue = Color(0xFF0047ff);
+const Color tdViolet = Color(0xFF948bb7);
+
+const Color tdGrey = Color(0xFF484554);
+const Color tdBlack = Color(0xFF312c4a);
+
+const Color tdBGColor = Color(0xFFfbf8ff);
---
+++ lib/screeen/home.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/constrants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildApp(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: Column(
+          children: [
+            searchBox(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: const TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(Icons.search, color: tdGrey, size: 20),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+            border: InputBorder.none,
+            hintText: 'Search',
+            hintStyle: TextStyle(color: tdGrey)),
+      ),
+    );
+  }
+
+  AppBar _buildApp() {
+    return AppBar(
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Icon(
+              Icons.menu,
+              color: tdBlue,
+              size: 30,
+            ),
+            SizedBox(
+                height: 40,
+                width: 40,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/images/icon.png'),
+                ))
+          ],
+        ));
+  }
+}
