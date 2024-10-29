6988662ac3660fcf1d77095268fd8fd0d45eae05
kop-san <anouson11103@gmail.com>
2024-10-29 11:35:33 +0700

    merge to main

--- lib/screeen/home.dart
+++ lib/screeen/home.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/constrants/colors.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
